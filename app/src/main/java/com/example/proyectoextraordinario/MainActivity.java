8c46419b80089811d0e5128a605e094a8f83bf48
aracavic <aracamatelmo@gmail.com>
2025-05-13 17:43:29 +0200

    toma de contacto despues de tiempo

--- app/src/main/java/com/example/proyectoextraordinario/MainActivity.java
+++ app/src/main/java/com/example/proyectoextraordinario/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.proyectoextraordinario;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Configuración de padding para las barras del sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Configuración del botón
+        Button button = findViewById(R.id.boton);
+        button.setOnClickListener(v ->
+                Toast.makeText(MainActivity.this, "Botón presionado", Toast.LENGTH_SHORT).show()
+        );
     }
 }
